a8023cf76bdd036ab103751741d7e6935968aac2
Gerson Raniery <gersonraniery12@gmail.com>
2022-09-20 16:54:54 -0300

    adicionando dados fake

--- recipes/views.py
+++ recipes/views.py
@@ -1,20 +1,19 @@
 from cgitb import html
 
 from django.shortcuts import render
-
-# from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 # Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Gerson Raniery'
+        'recipes': [make_recipe() for _ in range(10)],
     }
     )
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Gerson Raniery'
+        'recipe': make_recipe(),
     }
     )
