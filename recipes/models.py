6f4cc9019c8b68b708ede25c75465928c3745208
Gerson Raniery <gersonraniery12@gmail.com>
2022-09-22 16:59:02 -0300

    Criando models

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,25 @@
+from distutils.command.upload import upload
+
 from django.db import models
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+
 # Create your models here.
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+    discription = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preration_time = models.IntegerField()
+    preration_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
+    preration_steps = models.TextField()
+    preration_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
