95f46672ac1264a798b726ea01a5f620ce5d68bf
Gerson Raniery <gersonraniery12@gmail.com>
2022-09-29 18:09:11 -0300

    primeiro crud

--- recipes/models.py
+++ recipes/models.py
@@ -7,6 +7,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
+
 
 # Create your models here.
 
@@ -30,3 +33,6 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
+
+    def __str__(self):
+        return self.title
